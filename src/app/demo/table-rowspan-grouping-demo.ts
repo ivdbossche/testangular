5c4932ae96d9700ce44166b61ec64bc52eb2780f
ivdbossche <ivan.vandenbossche@involved-it.be>
2023-12-22 12:46:18 +0100

    Initial commit

---
+++ src/app/demo/table-rowspan-grouping-demo.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from '../../domain/customer';
+import { CustomerService } from '../../service/customerservice';
+
+@Component({
+    selector: 'table-rowspan-grouping-demo',
+    templateUrl: 'table-rowspan-grouping-demo.html'
+})
+export class TableRowspanGroupingDemo implements OnInit{
+    customers!: Customer[];
+
+    constructor(private customerService: CustomerService) {}
+
+    ngOnInit() {
+        this.customerService.getCustomersMedium().then((data) => {
+            this.customers = data;
+        });
+    }
+
+    calculateCustomerTotal(name: string) {
+        let total = 0;
+
+        if (this.customers) {
+            for (let customer of this.customers) {
+                if (customer.representative?.name === name) {
+                    total++;
+                }
+            }
+        }
+
+        return total;
+    }
+
+    getSeverity(status: string) {
+        switch (status) {
+            case 'unqualified':
+                return 'danger';
+
+            case 'qualified':
+                return 'success';
+
+            case 'new':
+                return 'info';
+
+            case 'negotiation':
+                return 'warning';
+
+            case 'renewal':
+                return null;
+        }
+    }
+}
